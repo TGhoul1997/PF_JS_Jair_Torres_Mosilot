79b073af5277ee4cd03bc8cc46d4982c58fb2d0e
TGhoul1997 <j.t.m_1997@outlook.com>
2024-06-19 22:28:22 -0500

    structure corrected

---
+++ js/cards.js
@@ -0,0 +1,23 @@
+const cards = [
+    {
+        id: 1,
+        b1: "Membresía anual S/ 80 (GRATIS si consumes S/1 al mes)",
+        b2: "1 Milla acumulada por cada 2 USD que consumas",
+        b3: "Campañas multiplicadoras de Millas",
+        b4: "Delivery gratis a donde quieras",
+    },
+    {
+        id: 1,
+        b1: "Membresía anual S/ 80 (GRATIS si consumes S/1 al mes)",
+        b2: "1 milla por cada USD 1.5 de consumo",
+        b3: "Campañas multiplicadoras de Millas",
+        b4: "Delivery gratis a donde quieras",
+    },
+    {
+        id: 1,
+        b1: "3,000 Millas de bienvenida",
+        b2: "Membresía anual S/350 (GRATIS si consumes S/1,200 al mes)",
+        b3: "1 Milla acumulada por cada 1 USD que consumas",
+        b4: "Delivery gratis a donde quieras",
+    },
+];
